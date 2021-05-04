1b3f2b0545858afedad7ee0cecda650b7a861857
L-Greco <makaronaskonstandinos@gmail.com>
2021-05-04 16:42:41 +0300

    constructor rdy

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -3,20 +3,31 @@
 
 // a functional component is just an arrow function returning JSX
 
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav } from "react-bootstrap";
+import { Component } from "react";
 
-const MyNavBar = (props) => (
-    <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">{props.title} - Strive For Food</Navbar.Brand>
+class MyNavBar extends Component {
+  constructor(props) {
+    super(props);
+    console.log("My navbar is loaded!");
+  }
+
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">
+          {this.props.title} - Strive For Food
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-                <Nav.Link>Menu</Nav.Link>
-                <Nav.Link>Reservations</Nav.Link>
-                <Nav.Link>Contact us</Nav.Link>
-            </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link>Menu</Nav.Link>
+            <Nav.Link>Reservations</Nav.Link>
+            <Nav.Link>Contact us</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
-    </Navbar>
-)
-
-export default MyNavBar
+      </Navbar>
+    );
+  }
+}
+export default MyNavBar;

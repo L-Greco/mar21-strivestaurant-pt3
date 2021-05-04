9b2717a1f7086f40332eb021e02a3e93a0185b32
L-Greco <makaronaskonstandinos@gmail.com>
2021-05-04 16:47:11 +0300

    added text if no comments

--- src/components/DishComments.jsx
+++ src/components/DishComments.jsx
@@ -7,11 +7,15 @@ class DishComments extends Component {
       <div className={`mt-${this.props.marginTop}`}>
         <h2>Comments for {this.props.dish.name}</h2>
         <ListGroup>
-          {this.props.dish.comments.map((c) => (
-            <ListGroup.Item key={c.id}>
-              "{c.comment}" from {c.author}
-            </ListGroup.Item>
-          ))}
+          {!this.props.dish.comments ? (
+            <p> No comments found!</p>
+          ) : (
+            this.props.dish.comments.map((c) => (
+              <ListGroup.Item key={c.id}>
+                "{c.comment}" from {c.author}
+              </ListGroup.Item>
+            ))
+          )}
         </ListGroup>
       </div>
     );
